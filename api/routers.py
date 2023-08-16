a4e98a9b4588061c52c409c1c5375cfdca5abf4c
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-08-16 18:27:58 +0700

    Add minor changes like title and tags Add ps to readme.md

--- api/routers.py
+++ api/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from clickhouse.crud import get_records
 
 
-api_router = APIRouter()
+api_router = APIRouter(tags=['ETGB'])
 
 @api_router.get('')
 async def get_ETGB():
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 
 from api.routers import api_router
 
-app = FastAPI()
+app = FastAPI(title='Elektronik Ticaret Gümrük Beyannamesi (ETGB) для продавцов из Турции.')
 app.include_router(api_router, prefix='/api')
